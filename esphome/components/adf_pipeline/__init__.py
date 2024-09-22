3a5f21f4a623e39418d6b151fd2b810d046a01ca
Anton Sergunov <setosha@gmail.com>
2024-09-23 00:36:30 +0600

    Update __init__.py

--- esphome/components/adf_pipeline/__init__.py
+++ esphome/components/adf_pipeline/__init__.py
@@ -105,7 +105,7 @@ async def to_code(config):
 
     esp32.add_extra_build_file(
         "esp_adf_patches/idf_v4.4_freertos.patch",
-        "https://github.com/espressif/esp-adf/raw/v2.5/idf_patches/idf_v4.4_freertos.patch",
+        "https://github.com/espressif/esp-adf/raw/v2.7/idf_patches/idf_v4.4_freertos.patch",
     )
 
     add_idf_component(
